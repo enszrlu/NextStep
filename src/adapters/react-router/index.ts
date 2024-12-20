d9a53b34b92e8fae8fe3930fbf9ca243aa120ed5
0ctan33 <76810766+0ctan33@users.noreply.github.com>
2024-12-20 13:39:15 +1100

    Fix window is not defined error

--- src/adapters/react-router/index.ts
+++ src/adapters/react-router/index.ts
@@ -7,12 +7,16 @@ import type { NavigationAdapter } from '../../types/navigation';
 export const useReactRouterAdapter = (): NavigationAdapter => {
   const navigate = useNavigate();
   const location = useLocation();
+  const isClient = typeof window !== 'undefined';
+
 
   return {
     push: (path: string) => {
+      if (isClient) {
       console.log('BETA: pushing react-router adapter', path);
       navigate(path);
+      }
     },
-    getCurrentPath: () => location.pathname,
+    getCurrentPath: () => (isClient ? location.pathname : '/'),
   };
 };
